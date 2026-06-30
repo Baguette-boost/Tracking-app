a8585643d885950f97048a2634121810865d567f
KyeongTaek <tommyholic@naver.com>
2026-06-30 23:58:29 +0900

    feat: add expo push notification

--- App.tsx
+++ App.tsx
@@ -3,11 +3,99 @@
 
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { useState, useEffect, useRef, React } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootTab from './src/navigation/RootTab';
+import { Text, View, Button, Platform } from 'react-native';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+
+// 포그라운드 상태일 때 알림 처리 방법
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+    }),
+});
+
+// 푸시 토큰 발급 함수
+async function registerForPushNotificationsAsync() {
+    let token;
+
+    if (Platform.OS === 'android') {
+        await Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+        });
+    }
+
+    if (Device.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+        }
+        // EAS 프로젝트 ID를 사용해 토큰 가져옴
+        const projectId = Constants.expoConfig?.extra?.eas?.projectId;
+        if (!projectId) {
+            alert('Project Id not found. Make sure you have configured it in app.json');
+            return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync({ projectId })).data;
+        console.log(token);
+    } else {
+        alert('Must use physical device for Push Notifications');
+    }
+
+    return token;
+}
 
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState<Notifications.Notification | false>(false);
+  const notificationListener = useRef<Notifications.Subscription>();
+  const responseListener = useRef<Notifications.Subscription>();
+
+  useEffect(() => {
+      registerForPushNotificationsAsync().then(token => setExpoPushToken(token ?? ''));
+
+//       // 앱 실행 중 알림 수신 시 발생하는 리스너
+//       notificationListener.current = Notifications.addNotifications.addNotificationReceivedListener(notification => {
+//           setNotification(notification);
+//       });
+//       // 사용자가 알림과 상호작용(ex. 탭)했을 때 발생하는 리스너
+//       responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+//           console.log(response);
+//           });
+//   return () => {
+//             if (notificationListener.current) {
+//               Notifications.removeNotificationSubscription(notificationListener.current);
+//             }
+//             if (responseListener.current) {
+//               Notifications.removeNotificationSubscription(responseListener.current);
+//             }
+//           };
+        }, []);
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around' }}>
+          <Text>Your Expo push token: {expoPushToken}</Text>
+          <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Title: {notification && notification.request.content.title} </Text>
+            <Text>Body: {notification && notification.request.content.body}</Text>
+            <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+          </View>
+        </View>
+      );
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
